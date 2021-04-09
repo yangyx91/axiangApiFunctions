9bd74cb32249ca73a9362e9e755c14a3a524811e
阿象 <yang.yx91@live.cn>
2021-04-09 15:47:50 +0800

    扩充deleteFile

---
+++ axiangwechat/uniCloud-aliyun/cloudfunctions/deleteFile/index.js
@@ -0,0 +1,58 @@
+'use strict';
+const {
+	formatDateToYYYYMMDD,
+	formatDateToYYYYMMDDHHMMSS
+}=require('uni-common');
+const db=uniCloud.database()
+exports.main = async (event, context) => {
+	let list=[];
+	let imgId="";
+	let imgUrl="";
+	let imgName="";
+	let imgBase64="";
+	let body = event.body
+	if(event.isBase64Encoded){
+	      body = Buffer.from(body)
+	}
+	if(body!=''){
+		try{
+			const param = JSON.parse(body);
+			//event为客户端上传的参数
+			
+			if(param.imgUrl!=undefined && param.imgUrl!=''){
+				imgUrl=param.imgUrl;
+				list.push(imgUrl);
+			}
+			
+		}
+		catch(e){
+			return {
+				status: -1,
+				msg: e.message
+			}
+		}
+	}
+	
+	try{
+		
+		if(imgUrl!=''&& list.length>0){
+			// 云函数删除文件示例代码
+			let result = await uniCloud.deleteFile({
+			    fileList: list
+			});
+			return result
+		}
+	}
+	catch(e){
+		return {
+			status: -1,
+			msg: e.message
+		}
+	}
+	return event;
+};
+
+
+//https://5f910eba-d66b-4a7f-803e-46465dd1179a.bspapp.com/http/deleteFile
+//https://openapi.axiangblog.com/deleteFile/v1/
+
